76e6afff68a251bf44b996f348be34006a765f00
Camila Piñero <cmlpr9@gmail.com>
2023-10-21 17:13:14 -0300

    Agrega carpeta routes y parte de swagger

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { createPool } from 'mysql2/promise';
-import empleadoRoutes from './routes/empleados.js';
+import express from "express";
+import { createPool } from "mysql2/promise";
+import empleadoRoutes from "./routes/empleado_routes.js";
 import swaggerUI from "swagger-ui-express";
 import swaggerJsDoc from "swagger-jsdoc";
 import { fileURLToPath } from "url";
@@ -11,20 +11,20 @@ const __dirname = path.dirname(__filename);
 
 // Define expecificaciones de Swagger
 const swaggerSpec = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Node Swagger API - Clase 39",
-            version: "1.0.0"
-        },
-        servers: [
-            {
-                url: "http://localhost:10000"
-            }
-        ]
-    },
-    apis: [`${path.join(__dirname, "./routes/*.js")}`]
-}
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Node Swagger API - Clase 39",
+			version: "1.0.0",
+		},
+		servers: [
+			{
+				url: "http://localhost:10000",
+			},
+		],
+	},
+	apis: [`${path.join(__dirname, "./routes/*.js")}`],
+};
 
 const app = express();
 const PORT = 10000;
@@ -44,21 +44,26 @@ export const pool = createPool(dbConfig);
 
 // Verificar conexión
 pool.getConnection()
-  .then((connection) => {
-    console.log('Conexión exitosa');
-    connection.release();
-  })
-  .catch((err) => {
-    console.error('Se produjo un error en la conexión a la base de datos:', err);
-  });
+	.then((connection) => {
+		console.log("Conexión exitosa");
+		connection.release();
+	})
+	.catch((err) => {
+		console.error(
+			"Se produjo un error en la conexión a la base de datos:",
+			err
+		);
+	});
 
-
-app.use('/', empleadoRoutes);
+app.use("/", empleadoRoutes);
 
 app.listen(PORT, () => {
-  console.log(`El servidor está escuchando en el puerto ${PORT}`);
+	console.log(`El servidor está escuchando en el puerto ${PORT}`);
 });
 
-
-app.use('/empleados', empleadoRoutes);
-app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
+app.use("/empleados", empleadoRoutes);
+app.use(
+	"/api-doc",
+	swaggerUI.serve,
+	swaggerUI.setup(swaggerJsDoc(swaggerSpec))
+);
